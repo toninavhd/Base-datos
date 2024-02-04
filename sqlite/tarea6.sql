bfab51c3d527308dc197a2dd734772bbe7c2b557
toni <toni@toni.toni>
2024-02-04 21:15:01 +0000

    tarea6e

--- sqlite/tarea6.sql
+++ sqlite/tarea6.sql
@@ -382,52 +382,337 @@ SELECT * FROM pedidos WHERE id_cliente = 1 AND strftime('%Y-%m', fecha_pedido) =
 └───────────┴────────────┴─────────────┴──────────┴──────────────┘
 
 -- Obtener la cantidad total de productos en todos los pedidos por producto ordenado por total de productos descendente.
-SELECT producto_id, SUM(cantidad) FROM pedidos GROUP BY producto_id ORDER BY SUM(cantidad) DESC;
+
+SELECT id_producto, SUM(cantidad) FROM pedidos GROUP BY id_producto ORDER BY SUM(cantidad) DESC;
+┌─────────────┬───────────────┐
+│ id_producto │ SUM(cantidad) │
+├─────────────┼───────────────┤
+│ 30          │ 3             │
+│ 25          │ 3             │
+│ 21          │ 3             │
+│ 16          │ 3             │
+│ 12          │ 3             │
+│ 7           │ 3             │
+│ 3           │ 3             │
+│ 28          │ 2             │
+│ 26          │ 2             │
+│ 23          │ 2             │
+│ 19          │ 2             │
+│ 17          │ 2             │
+│ 14          │ 2             │
+│ 10          │ 2             │
+│ 8           │ 2             │
+│ 5           │ 2             │
+│ 1           │ 2             │
+│ 29          │ 1             │
+│ 27          │ 1             │
+│ 24          │ 1             │
+│ 22          │ 1             │
+│ 20          │ 1             │
+│ 18          │ 1             │
+│ 15          │ 1             │
+│ 13          │ 1             │
+│ 11          │ 1             │
+│ 9           │ 1             │
+│ 6           │ 1             │
+│ 4           │ 1             │
+│ 2           │ 1             │
+└─────────────┴───────────────┘
+
 
 -- Obtener los productos que no tienen un precio registrado.
 SELECT * FROM productos WHERE precio IS NULL;
 
 -- Obtener la fecha del último pedido realizado.
+
 SELECT MAX(fecha) FROM pedidos;
+┌───────────────────┐
+│ MAX(fecha_pedido) │
+├───────────────────┤
+│ 2024-03-01        │
+└───────────────────┘
 
 -- Obtener los clientes cuyo nombre tiene al menos 5 caracteres.
 SELECT * FROM clientes WHERE LENGTH(nombre) >= 5;
+┌────┬─────────────────┬───────────────────────────┐
+│ id │     nombre      │           email           │
+├────┼─────────────────┼───────────────────────────┤
+│ 1  │ Juan Pérez      │ juan@example.com          │
+│ 2  │ María Gómez     │ maria@example.com         │
+│ 3  │ Carlos López    │ carlos@example.com        │
+│ 4  │ Ana Rodríguez   │ ana@example.com           │
+│ 5  │ Luisa Martínez  │ luisa@example.com         │
+│ 6  │ Pedro Sánchez   │ pedro@example.com         │
+│ 7  │ Laura García    │ laura@example.com         │
+│ 8  │ Miguel Martín   │ miguel@example.com        │
+│ 9  │ Elena González  │ elena@example.com         │
+│ 10 │ David Torres    │ david@example.com         │
+│ 11 │ Sofía Ruiz      │ sofia@example.com         │
+│ 12 │ Javier López    │ javier@example.com        │
+│ 13 │ Carmen Vargas   │ carmen@example.com        │
+│ 14 │ Daniel Muñoz    │ daniel@example.com        │
+│ 15 │ Isabel Serrano  │ isabel@example.com        │
+│ 16 │ Alejandro Muñoz │ alejandro@example.com     │
+│ 17 │ Raquel Herrera  │ raquel@example.com        │
+│ 18 │ Francisco Mora  │ francisco@example.com     │
+│ 19 │ Marina Díaz     │ marina@example.com        │
+│ 20 │ Antonio Jiménez │ antonio@example.com       │
+│ 21 │ Beatriz Romero  │ beatriz@example.com       │
+│ 22 │ Carlos Gómez    │ carlos.gomez@example.com  │
+│ 23 │ Clara Sánchez   │ clara.sanchez@example.com │
+│ 24 │ Andrés Martínez │ andres@example.com        │
+│ 25 │ Lucía Díaz      │ lucia@example.com         │
+│ 26 │ Mario Serrano   │ mario@example.com         │
+│ 27 │ Eva Torres      │ eva.torres@example.com    │
+│ 28 │ Roberto Ruiz    │ roberto@example.com       │
+│ 29 │ Celia García    │ celia@example.com         │
+└────┴─────────────────┴───────────────────────────┘
 
 -- Obtener los productos que tienen la letra 'o' en cualquier posición del nombre.
 SELECT * FROM productos WHERE nombre LIKE '%o%';
 
+┌────┬───────────────────────────────────┬────────┐
+│ id │              nombre               │ precio │
+├────┼───────────────────────────────────┼────────┤
+│ 1  │ Laptop                            │ 1200.0 │
+│ 2  │ Smartphone                        │ 699.99 │
+│ 5  │ Auriculares Bluetooth             │ 79.99  │
+│ 6  │ Impresora                         │ 199.99 │
+│ 8  │ Reproductor de Audio              │ 149.99 │
+│ 9  │ Altavoces Inalámbricos            │ 129.99 │
+│ 10 │ Reloj Inteligente                 │ 249.99 │
+│ 11 │ Teclado Inalámbrico               │ 59.99  │
+│ 12 │ Ratón Óptico                      │ 29.99  │
+│ 13 │ Monitor LED                       │ 349.99 │
+│ 14 │ Mochila para Portátil             │ 49.99  │
+│ 15 │ Disco Duro Externo                │ 89.99  │
+│ 16 │ Router Wi-Fi                      │ 69.99  │
+│ 20 │ Tarjeta de Memoria                │ 24.99  │
+│ 21 │ Cargador Inalámbrico              │ 34.99  │
+│ 22 │ Kit de Limpieza para Computadoras │ 9.99   │
+│ 24 │ Soporte para Teléfono             │ 14.99  │
+│ 27 │ Funda para Laptop                 │ 29.99  │
+│ 28 │ Adaptador HDMI                    │ 12.99  │
+└────┴───────────────────────────────────┴────────┘
+
+
 -- Obtener la cantidad total de productos en todos los pedidos por cliente ordenado por cliente.
+
 SELECT id_cliente, SUM(cantidad) FROM pedidos GROUP BY id_cliente ORDER BY id_cliente;
+┌────────────┬───────────────┐
+│ id_cliente │ SUM(cantidad) │
+├────────────┼───────────────┤
+│ 1          │ 2             │
+│ 2          │ 1             │
+│ 3          │ 3             │
+│ 4          │ 1             │
+│ 5          │ 2             │
+│ 6          │ 1             │
+│ 7          │ 3             │
+│ 8          │ 2             │
+│ 9          │ 1             │
+│ 10         │ 2             │
+│ 11         │ 1             │
+│ 12         │ 3             │
+│ 13         │ 1             │
+│ 14         │ 2             │
+│ 15         │ 1             │
+│ 16         │ 3             │
+│ 17         │ 2             │
+│ 18         │ 1             │
+│ 19         │ 2             │
+│ 20         │ 1             │
+│ 21         │ 3             │
+│ 22         │ 1             │
+│ 23         │ 2             │
+│ 24         │ 1             │
+│ 25         │ 3             │
+│ 26         │ 2             │
+│ 27         │ 1             │
+│ 28         │ 2             │
+│ 29         │ 1             │
+│ 30         │ 3             │
+└────────────┴───────────────┘
 
 -- Obtener los clientes cuyos nombres no contienen la letra 'i':
+
 SELECT * FROM clientes WHERE nombre NOT LIKE '%i%';
+┌────┬─────────────────┬───────────────────────────┐
+│ id │     nombre      │           email           │
+├────┼─────────────────┼───────────────────────────┤
+│ 1  │ Juan Pérez      │ juan@example.com          │
+│ 2  │ María Gómez     │ maria@example.com         │
+│ 3  │ Carlos López    │ carlos@example.com        │
+│ 4  │ Ana Rodríguez   │ ana@example.com           │
+│ 6  │ Pedro Sánchez   │ pedro@example.com         │
+│ 7  │ Laura García    │ laura@example.com         │
+│ 9  │ Elena González  │ elena@example.com         │
+│ 13 │ Carmen Vargas   │ carmen@example.com        │
+│ 16 │ Alejandro Muñoz │ alejandro@example.com     │
+│ 17 │ Raquel Herrera  │ raquel@example.com        │
+│ 22 │ Carlos Gómez    │ carlos.gomez@example.com  │
+│ 23 │ Clara Sánchez   │ clara.sanchez@example.com │
+│ 24 │ Andrés Martínez │ andres@example.com        │
+│ 25 │ Lucía Díaz      │ lucia@example.com         │
+│ 27 │ Eva Torres      │ eva.torres@example.com    │
+└────┴─────────────────┴───────────────────────────┘
 
 -- Obtener los pedidos realizados por el cliente con ID 2 en febrero de 2024.
-SELECT * FROM pedidos WHERE id_cliente = 2 AND strftime('%Y-%m', fecha) = '2024-02';
+
+SELECT * FROM pedidos WHERE id_cliente = 2 AND strftime('%Y-%m', fecha_pedido) = '2024-02';
+┌───────────┬────────────┬─────────────┬──────────┬──────────────┐
+│ id_pedido │ id_cliente │ id_producto │ cantidad │ fecha_pedido │
+├───────────┼────────────┼─────────────┼──────────┼──────────────┤
+│ 2         │ 2          │ 2           │ 1        │ 2024-02-02   │
+└───────────┴────────────┴─────────────┴──────────┴──────────────┘
 
 -- Obtener el precio mínimo de los productos.
+
 SELECT MIN(precio) FROM productos;
+┌─────────────┐
+│ MIN(precio) │
+├─────────────┤
+│ 9.99        │
+└─────────────┘
 
 -- Obtener los clientes que han realizado al menos un pedido en febrero de 2024.
-SELECT DISTINCT id_cliente FROM pedidos WHERE strftime('%Y-%m', fecha) = '2024-02';
+
+SELECT DISTINCT id_cliente FROM pedidos WHERE strftime('%Y-%m', fecha_pedido) = '2024-02';
+
+┌────────────┐
+│ id_cliente │
+├────────────┤
+│ 1          │
+│ 2          │
+│ 3          │
+│ 4          │
+│ 5          │
+│ 6          │
+│ 7          │
+│ 8          │
+│ 9          │
+│ 10         │
+│ 11         │
+│ 12         │
+│ 13         │
+│ 14         │
+│ 15         │
+│ 16         │
+│ 17         │
+│ 18         │
+│ 19         │
+│ 20         │
+│ 21         │
+│ 22         │
+│ 23         │
+│ 24         │
+│ 25         │
+│ 26         │
+│ 27         │
+│ 28         │
+│ 29         │
+└────────────┘
+
 
 -- Obtener la fecha del último pedido realizado por el cliente con ID 3.
-SELECT MAX(fecha) FROM pedidos WHERE id_cliente = 3;
+SELECT MAX(fecha_pedido) FROM pedidos WHERE id_cliente = 3;
+┌───────────────────┐
+│ MAX(fecha_pedido) │
+├───────────────────┤
+│ 2024-02-03        │
+└───────────────────┘
+
 
 -- Obtener los productos que tienen una 'a' al final del nombre.
 SELECT * FROM productos WHERE nombre LIKE '%a';
 
+┌────┬────────────────────┬────────┐
+│ id │       nombre       │ precio │
+├────┼────────────────────┼────────┤
+│ 6  │ Impresora          │ 199.99 │
+│ 18 │ Batería Externa    │ 19.99  │
+│ 20 │ Tarjeta de Memoria │ 24.99  │
+└────┴────────────────────┴────────┘
+
 -- Obtener los clientes cuyos nombres tienen al menos 4 vocales (mayúsculas|minúsculas).
-SELECT * FROM clientes WHERE (nombre GLOB '*[aeiouAEIOU]*') AND (nombre GLOB '*[aeiouAEIOU]*') AND (nombre GLOB '*[aeiouAEIOU]*') AND (nombre GLOB '*[aeiouAEIOU]*');
+SELECT * FROM clientes WHERE nombre REGEXP '[aeiouAEIOU].*[aeiouAEIOU]';
+
+┌────┬─────────────────┬───────────────────────────┐
+│ id │     nombre      │           email           │
+├────┼─────────────────┼───────────────────────────┤
+│ 1  │ Juan Pérez      │ juan@example.com          │
+│ 2  │ María Gómez     │ maria@example.com         │
+│ 3  │ Carlos López    │ carlos@example.com        │
+│ 4  │ Ana Rodríguez   │ ana@example.com           │
+│ 5  │ Luisa Martínez  │ luisa@example.com         │
+│ 6  │ Pedro Sánchez   │ pedro@example.com         │
+│ 7  │ Laura García    │ laura@example.com         │
+│ 8  │ Miguel Martín   │ miguel@example.com        │
+│ 9  │ Elena González  │ elena@example.com         │
+│ 10 │ David Torres    │ david@example.com         │
+│ 11 │ Sofía Ruiz      │ sofia@example.com         │
+│ 12 │ Javier López    │ javier@example.com        │
+│ 13 │ Carmen Vargas   │ carmen@example.com        │
+│ 14 │ Daniel Muñoz    │ daniel@example.com        │
+│ 15 │ Isabel Serrano  │ isabel@example.com        │
+│ 16 │ Alejandro Muñoz │ alejandro@example.com     │
+│ 17 │ Raquel Herrera  │ raquel@example.com        │
+│ 18 │ Francisco Mora  │ francisco@example.com     │
+│ 19 │ Marina Díaz     │ marina@example.com        │
+│ 20 │ Antonio Jiménez │ antonio@example.com       │
+│ 21 │ Beatriz Romero  │ beatriz@example.com       │
+│ 22 │ Carlos Gómez    │ carlos.gomez@example.com  │
+│ 23 │ Clara Sánchez   │ clara.sanchez@example.com │
+│ 24 │ Andrés Martínez │ andres@example.com        │
+│ 25 │ Lucía Díaz      │ lucia@example.com         │
+│ 26 │ Mario Serrano   │ mario@example.com         │
+│ 27 │ Eva Torres      │ eva.torres@example.com    │
+│ 28 │ Roberto Ruiz    │ roberto@example.com       │
+│ 29 │ Celia García    │ celia@example.com         │
+└────┴─────────────────┴───────────────────────────┘
 
 -- Obtener los productos cuyo precio tenga al menos 4 números sin contar los decimales.
+
 SELECT * FROM productos WHERE CAST(precio AS INTEGER) >= 1000;
+┌────┬────────┬────────┐
+│ id │ nombre │ precio │
+├────┼────────┼────────┤
+│ 1  │ Laptop │ 1200.0 │
+└────┴────────┴────────┘
 
 -- Obtener los clientes cuyos nombres tienen al menos una 'a' seguida de una 'e'.
 SELECT * FROM clientes WHERE nombre LIKE '%ae%';
 
 -- Obtener los productos cuyos nombres terminan con una consonante.
-SELECT * FROM productos WHERE nombre GLOB '*[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]';
+SELECT * FROM productos WHERE nombre REGEXP '[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]$';
+┌────┬───────────────────────────────────┬────────┐
+│ id │              nombre               │ precio │
+├────┼───────────────────────────────────┼────────┤
+│ 1  │ Laptop                            │ 1200.0 │
+│ 3  │ TV LED                            │ 799.5  │
+│ 4  │ Tablet                            │ 299.99 │
+│ 5  │ Auriculares Bluetooth             │ 79.99  │
+│ 7  │ Cámara Digital                    │ 499.99 │
+│ 9  │ Altavoces Inalámbricos            │ 129.99 │
+│ 13 │ Monitor LED                       │ 349.99 │
+│ 14 │ Mochila para Portátil             │ 49.99  │
+│ 17 │ Lámpara LED                       │ 39.99  │
+│ 19 │ Estuche para Auriculares          │ 14.99  │
+│ 22 │ Kit de Limpieza para Computadoras │ 9.99   │
+│ 23 │ Funda para Tablet                 │ 19.99  │
+│ 25 │ Hub USB                           │ 29.99  │
+│ 26 │ Webcam HD                         │ 59.99  │
+│ 27 │ Funda para Laptop                 │ 29.99  │
+└────┴───────────────────────────────────┴────────┘
 
 -- Obtener los productos cuyos nombres empiezan con una vocal.
-SELECT * FROM productos WHERE nombre GLOB '[aeiouAEIOU]*';
+SELECT * FROM productos WHERE nombre REGEXP '^[aeiouAEIOU]';
+┌────┬──────────────────────────┬────────┐
+│ id │          nombre          │ precio │
+├────┼──────────────────────────┼────────┤
+│ 5  │ Auriculares Bluetooth    │ 79.99  │
+│ 6  │ Impresora                │ 199.99 │
+│ 9  │ Altavoces Inalámbricos   │ 129.99 │
+│ 19 │ Estuche para Auriculares │ 14.99  │
+│ 28 │ Adaptador HDMI           │ 12.99  │
+└────┴──────────────────────────┴────────┘
