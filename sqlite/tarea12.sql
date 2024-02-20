481dc08e3ba4f281233435dd5ac4ffdff2fc3d08
toninavhd <tillidie9999@gmail.com>
2024-02-20 16:09:16 +0000

    tarea12

--- sqlite/tarea12.sql
+++ sqlite/tarea12.sql
@@ -3,8 +3,62 @@
 ### TAREA 12 ###
 ################
 **/
+/**
+##############
+### Tablas ###
+##############
+**/
 
+-- Clientes
+┌────┬───────────┬───────────┬───────────┬─────────┬───────────┐
+│ ID │  nombre   │ apellido1 │ apellido2 │ ciudad  │ categoria │
+├────┼───────────┼───────────┼───────────┼─────────┼───────────┤
+│ 1  │ Aarón     │ Rivero    │ Gómez     │ Almería │ 100       │
+│ 2  │ Adela     │ Salas     │ Díaz      │ Granada │ 200       │
+│ 3  │ Adolfo    │ Rubio     │ Flores    │ Sevilla │           │
+│ 4  │ Adrián    │ Suárez    │           │ Jaén    │ 300       │
+│ 5  │ Marcos    │ Loyola    │ Méndez    │ Almería │ 200       │
+│ 6  │ María     │ Santana   │ Moreno    │ Cádiz   │ 100       │
+│ 7  │ Pilar     │ Ruiz      │           │ Sevilla │ 300       │
+│ 8  │ Pepe      │ Ruiz      │ Santana   │ Huelva  │ 200       │
+│ 9  │ Guillermo │ López     │ Gómez     │ Granada │ 225       │
+│ 10 │ Daniel    │ Santana   │ Loyola    │ Sevilla │ 125       │
+└────┴───────────┴───────────┴───────────┴─────────┴───────────┘
+-- Comercial
+┌────┬─────────┬───────────┬───────────┬───────────┐
+│ ID │ nombre  │ apellido1 │ apellido2 │ categoria │
+├────┼─────────┼───────────┼───────────┼───────────┤
+│ 1  │ Daniel  │ Sáez      │ Vega      │ 0.15      │
+│ 2  │ Juan    │ Gómez     │ López     │ 0.13      │
+│ 3  │ Diego   │ Flores    │ Salas     │ 0.11      │
+│ 4  │ Marta   │ Herrera   │ Gil       │ 0.14      │
+│ 5  │ Antonio │ Carretero │ Ortega    │ 0.12      │
+│ 6  │ Manuel  │ Domínguez │ Hernández │ 0.13      │
+│ 7  │ Antonio │ Vega      │ Hernández │ 0.11      │
+│ 8  │ Alfredo │ Ruiz      │ Flores    │ 0.05      │
+└────┴─────────┴───────────┴───────────┴───────────┘
 
+-- Pedido
+┌────┬─────────┬────────────┬────────────┬──────────────┐
+│ ID │  total  │   fecha    │ id_cliente │ id_comercial │
+├────┼─────────┼────────────┼────────────┼──────────────┤
+│ 1  │ 150.5   │ 2017-10-05 │ 5          │ 2            │
+│ 2  │ 270.65  │ 2016-09-10 │ 1          │ 5            │
+│ 3  │ 65.26   │ 2017-10-05 │ 2          │ 1            │
+│ 4  │ 110.5   │ 2016-08-17 │ 8          │ 3            │
+│ 5  │ 948.5   │ 2017-09-10 │ 5          │ 2            │
+│ 6  │ 2400.6  │ 2016-07-27 │ 7          │ 1            │
+│ 7  │ 5760.0  │ 2015-09-10 │ 2          │ 1            │
+│ 8  │ 1983.43 │ 2017-10-10 │ 4          │ 6            │
+│ 9  │ 2480.4  │ 2016-10-10 │ 8          │ 3            │
+│ 10 │ 250.45  │ 2015-06-27 │ 8          │ 2            │
+│ 11 │ 75.29   │ 2016-08-17 │ 3          │ 7            │
+│ 12 │ 3045.6  │ 2017-04-25 │ 2          │ 1            │
+│ 13 │ 545.75  │ 2019-01-25 │ 6          │ 1            │
+│ 14 │ 145.82  │ 2017-02-02 │ 6          │ 1            │
+│ 15 │ 370.85  │ 2019-03-11 │ 1          │ 5            │
+│ 16 │ 2389.23 │ 2019-03-11 │ 1          │ 5            │
+└────┴─────────┴────────────┴────────────┴──────────────┘
 /**
 #############################
 ### Consultas a una tabla ###
@@ -123,8 +177,6 @@
 ##    Subconsultas con EXIST y NOT IN    ##
 ###########################################
 **/
-
-
 
 -- Devuelve un listado de los clientes que no han realizado ningún pedido. (Utilizando EXISTS o NOT EXISTS).
 
